7339346ef5e6b7fa8d8cf887d2d458d0bea473e0
icodelakshay <lakshaysaxena2217@gmail.com>
2025-09-01 12:41:13 +0530

    fix: update CORS allowedHeaders to accept all headers

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders:"*"
 };
 // Middleware
 app.use(cors(corsOptions));
